c0f5558453a70b49e4b602d42524596f62f23c93
HasyidRasy <hasyidrasyidi@gmail.com>
2023-10-16 13:23:29 +0700

    Add dragscroll to Cards component

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -36,25 +36,15 @@ onMounted(async () => {
 <template>
   <div class="mb-4 d-md-none" v-for="(platf, platfIndex) in data.platform">
     <h4>{{ platf }}</h4>
-    <div class="d-flex overflow-x-auto" style="scrollbar-width: thin">
-      <div
-        class="card m-3"
-        style="min-width: 200px"
-        v-for="(time, timeIndex) in data.time"
-      >
+    <div v-dragscroll class="d-flex overflow-x-auto">
+      <div class="card m-3" style="min-width: 200px" v-for="(time, timeIndex) in data.time">
         <div class="card-body">
           <h5 class="card-title text-center">{{ time }}</h5>
           <template v-for="(type, typeIndex) in data.type">
             <div class="d-flex justify-content-between">
               <p class="card-text">{{ type }}</p>
               <p>
-                <b>{{
-                  data.data[
-                    timeIndex +
-                      data.time.length * typeIndex * data.platform.length +
-                      platfIndex * data.time.length
-                  ].total
-                }}</b>
+                <b>{{ data.data[timeIndex + data.time.length * typeIndex * data.platform.length + platfIndex * data.time.length].total }}</b>
               </p>
             </div>
           </template>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import 'vue-toast-notification/dist/theme-bootstrap.css';
-import '@vuepic/vue-datepicker/dist/main.css'
-import './assets/main.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "vue-toast-notification/dist/theme-bootstrap.css";
+import "@vuepic/vue-datepicker/dist/main.css";
+import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
-import ToastPlugin from 'vue-toast-notification'
-import VueDatePicker from '@vuepic/vue-datepicker';
+import App from "./App.vue";
+import router from "./router";
+import ToastPlugin from "vue-toast-notification";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import VueDragscroll from "vue-dragscroll";
 
 const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(ToastPlugin)
-app.component('VueDatePicker', VueDatePicker);
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+app.use(ToastPlugin);
+app.use(VueDragscroll);
+app.component("VueDatePicker", VueDatePicker);
+app.mount("#app");
